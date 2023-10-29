7781acf05cd8d868dc76eb2bd74164189db14ef0
minnim1010 <pms1010123@daum.net>
2023-10-29 23:26:46 +0900

    refactor: 메시지 상수 위치 이동
    Why: 한 번만 사용되므로 클래스 분리할 필요 없다고 판단

--- src/main/java/baseball/view/BaseballView.java
+++ src/main/java/baseball/view/BaseballView.java
@@ -1,7 +1,6 @@
 package baseball.view;
 
 import baseball.common.constants.GameResultMessage;
-import baseball.common.constants.Message;
 import baseball.dto.input.BaseballDto;
 import baseball.dto.input.ReplayChoiceDto;
 import baseball.dto.output.GameResultDto;
@@ -13,17 +12,22 @@ public class BaseballView {
     private final Reader reader;
     private final Writer writer;
 
+    public static final String START_GAME = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    public static final String GAME_CLEAR = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String INPUT_REPLAY = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
     public BaseballView(Reader reader, Writer writer) {
         this.reader = reader;
         this.writer = writer;
     }
 
     public void startGame() {
-        writer.writeLine(Message.START_GAME);
+        writer.writeLine(START_GAME);
     }
 
     public BaseballDto inputNumber() {
-        writer.write(Message.INPUT_NUMBER);
+        writer.write(INPUT_NUMBER);
         return new BaseballDto(reader.readLine());
     }
 
@@ -33,11 +37,11 @@ public class BaseballView {
     }
 
     public void clearGame() {
-        writer.writeLine(Message.GAME_CLEAR);
+        writer.writeLine(GAME_CLEAR);
     }
 
     public ReplayChoiceDto replayGame() {
-        writer.writeLine(Message.INPUT_REPLAY);
+        writer.writeLine(INPUT_REPLAY);
         return new ReplayChoiceDto(reader.readLine());
     }
 }
